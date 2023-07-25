a00b21101e481b5b06652b52784c41e0e0ae936f
jswearingen1967 <140343635+jswearingen1967@users.noreply.github.com>
2023-07-25 17:49:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.title.center('Breakfast Menu')
+streamlit.title('Breakfast Menu')
 
 streamlit.title('Omega 3 and Blueberry Oatmeal')
 streamlit.title('Kale, Spinach & Rocket Smoothie')
