95fcd29b1e4e0b9c1e697f91486593313d860941
jswearingen1967 <140343635+jswearingen1967@users.noreply.github.com>
2023-07-27 17:23:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,14 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display Fruityvice API response
 streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+# import requests
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # Remove streamlit.text(fruityvice_response)
 
 # write your own comment -what does the next line do? 
@@ -32,8 +37,5 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
